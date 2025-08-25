5c9d324d37f143936411e4de0f61b9c38274bf9e
Maria Velasquez <mi.velasqueza1@uniandes.edu.co>
2025-08-25 18:09:26 -0500

    cambios

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": "proyecto",
+        "USER": "mivelasqueza1",
+        "PASSWORD": "saporaton25*",
         "HOST": "localhost",
-        "PORT": "",
+        "PORT": "5432",
     }
 }
 
